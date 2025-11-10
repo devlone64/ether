95fe864b7f6b9ab4b246691a0e97d06bad8df4cf
devlone64 <platlwlee1@gmail.com>
2025-11-10 17:40:06 +0900

    1.0.3: api의 maven 등록 문제 발견 후 수정.

--- ether-publish/build.gradle.kts
+++ ether-publish/build.gradle.kts
@@ -1,5 +1,6 @@
+import java.util.Locale
+
 plugins {
-    signing
     id("maven-publish")
 }
 
@@ -17,17 +18,41 @@ publishing {
     publications {
         fun MavenPublication.createPublication(project: Project) {
             groupId = "$group"
-            artifactId = rootProject.name.toLowerCase()
-            from(components["java"])
+            artifactId = project.name.lowercase(Locale.getDefault())
+            from(project.components["java"])
+
+            pom {
+                name.set(project.name.lowercase(Locale.getDefault()))
+                description.set("SpigotMC extension library written in Java")
+                url.set("https://github.com/devlone64/${rootProject.name}")
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License version 3")
+                        url.set("https://opensource.org/licenses/GPL-3.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("lone64")
+                        name.set("Lone64")
+                        url.set("https://github.com/devlone64")
+                        roles.addAll("developer")
+                        timezone.set("Asia/Seoul")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/devlone64/${rootProject.name}")
+                    connection.set("scm:git:git://github.com/devlone64/${rootProject.name}.git")
+                    developerConnection.set("scm:git:ssh://github.com:devlone64/${rootProject.name}.git")
+                }
+            }
         }
 
         create<MavenPublication>("api") {
             createPublication(projectAPI)
         }
     }
-}
-
-signing {
-    isRequired = true
-    sign(publishing.publications["api"])
 }
